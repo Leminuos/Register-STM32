83aef55ec8a71b7ffce9bbca6d80709b95ba8673
BuiNguyen <nguyenbui2804@gmail.com>
2025-04-09 03:17:59 -0700

    Clean code

--- APP/src/button_conf.c
+++ APP/src/button_conf.c
@@ -27,3 +27,93 @@ const BUTTON_CONFIG  ButtonConfigs[BUTTON_NUMBER] = {
         GPIO_PULLUP,
     }
 };
+
+void HandleButtonEvent(uint8_t evt, uint8_t btn)
+{
+    switch (btn)
+    {
+        case 0:
+        {
+            switch (evt)
+            {
+                case BUTTON_CLICK_EVENT:
+                GPIOA->ODR.BITS.ODR6 = 1;
+                break;
+        
+                case BUTTON_RELEASE_EVENT:
+                GPIOA->ODR.BITS.ODR6 = 0;
+                break;
+        
+                case BUTTON_LONG_PRESS_EVENT:
+                GPIOA->ODR.BITS.ODR6 = 0;
+                GPIOA->ODR.BITS.ODR7 = 1;
+                break;
+        
+                default:
+        
+                break;
+            }
+        }
+        break;
+
+        case 1:
+        {
+            switch (evt)
+            {
+                case BUTTON_CLICK_EVENT:
+                GPIOA->ODR.BITS.ODR7 = 1;
+                break;
+        
+                case BUTTON_RELEASE_EVENT:
+                GPIOA->ODR.BITS.ODR7 = 0;
+                break;
+        
+                default:
+        
+                break;
+            }
+        }
+        break;
+
+        case 2:
+        {
+            switch (evt)
+            {
+                case BUTTON_CLICK_EVENT:
+                GPIOB->ODR.BITS.ODR0 = 1;
+                break;
+        
+                case BUTTON_RELEASE_EVENT:
+                GPIOB->ODR.BITS.ODR0 = 0;
+                break;
+        
+                default:
+        
+                break;
+            }
+        }
+        break;
+
+        case 3:
+        {
+            switch (evt)
+            {
+                case BUTTON_CLICK_EVENT:
+                GPIOA->ODR.BITS.ODR6 = 1;
+                break;
+        
+                case BUTTON_RELEASE_EVENT:
+                GPIOA->ODR.BITS.ODR6 = 0;
+                break;
+        
+                default:
+        
+                break;
+            }
+        }
+        break;
+
+        default:
+        break;
+    }
+}

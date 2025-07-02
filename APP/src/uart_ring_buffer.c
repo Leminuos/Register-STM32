7cf88b6ed789fc175689016d8ad158168255764f
Leminous <buinguyen2804@gmail.com>
2025-07-02 22:59:03 +0700

    UART Ring buffer

--- APP/src/uart_ring_buffer.c
+++ APP/src/uart_ring_buffer.c
@@ -1,2 +1,43 @@
 #include "uart_ring_buffer.h"
+#include "stm32_driver_uart.h"
+
+ring_buffer_t r;
+
+uint8_t ring_buffer_empty(void)
+{
+    return r.pread == r.pwrite;
+}
+
+uint8_t ring_buffer_full(void)
+{
+    return ((r.pwrite + 1) % RING_BUFFER_SIZE) == r.pread;
+}
+
+uint8_t ring_buffer_put(char c)
+{
+    if (ring_buffer_full() != 0) return STATE_RING_BUFFER_FULL;
+
+    r.buffer[r.pwrite] = c;
+    r.pwrite = (++r.pwrite) % RING_BUFFER_SIZE;
+
+    return STATE_RING_BUFFER_SUCCESS;
+}
+
+uint8_t ring_buffer_get(char* c)
+{
+    if (ring_buffer_empty() != 0) return STATE_RING_BUFFER_EMPTY;
+
+    *c = r.buffer[r.pread];
+    r.pread = (++r.pread) % RING_BUFFER_SIZE;
+
+    return STATE_RING_BUFFER_SUCCESS;
+}
+
+void ring_buffer_dump(void)
+{
+    char c = 0;
+
+    while (ring_buffer_get(&c) == STATE_RING_BUFFER_SUCCESS)
+        USART_Transmiter(USART1, c);
+}
 
--- APP/src/uart_ring_buffer.h
+++ APP/src/uart_ring_buffer.h
@@ -3,4 +3,24 @@
 
 #include <stdint.h>
 
+#define STATE_RING_BUFFER_SUCCESS   0x00
+#define STATE_RING_BUFFER_FULL      0x01
+#define STATE_RING_BUFFER_EMPTY     0x02
+
+
+#define RING_BUFFER_SIZE            0x400 
+
+typedef struct {
+    uint32_t    pread;
+    uint32_t    pwrite;
+    char        buffer[RING_BUFFER_SIZE];
+} ring_buffer_t;
+
+extern void ring_buffer_dump(void);
+extern uint8_t ring_buffer_empty(void);
+extern uint8_t ring_buffer_full(void);
+extern uint8_t ring_buffer_put(char c);
+extern uint8_t ring_buffer_get(char* c);
+
 #endif  /* __UART_RING_BUFFER__ */
+
